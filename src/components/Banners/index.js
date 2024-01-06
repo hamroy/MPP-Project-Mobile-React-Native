d7ac9552187298a2f0f1d6fac416d2af98f1fc00
hamroy <hamroy1228@gmail.com>
2024-01-06 14:59:23 +0700

    banner

--- src/components/Banners/index.js
+++ src/components/Banners/index.js
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, StyleSheet, FlatList, Image, Text} from 'react-native';
-import {COLORS, SIZES, images} from '../../constants';
-const data = [
-  {
-    id: 1,
-    img: 'https://mpp.natunakab.go.id/admin/uploads/Icon_Logo_MPP1.png',
-  },
-  {
-    id: 2,
-    img: 'https://mpp.natunakab.go.id//admin//uploads//Icon_Logo_MPP1.png',
-  },
-];
-
-const SlideItem = ({item}) => {
-  return (
-    <View style={styles.bannerItem}>
-      <Image
-        // source={{
-        //   uri: item.img,
-        // }}
-        source={images.logo_fs}
-        resizeMode="contain"
-        style={styles.img}
-      />
-    </View>
-  );
-};
+import {COLORS, SIZES} from '../../constants';
 
 const Banners = () => {
+  const [data, setData] = useState([]);
+  const [widthBanner, setwidthBanner] = useState(1);
+
+  const getDataBanner = async () => {
+    try {
+      const response = await fetch(
+        'https://mpp.natunakab.go.id/admin/banner-json',
+      );
+      const json = await response.json();
+      setData(json);
+      if (json.length > 1) {
+        setwidthBanner(0.9);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getDataBanner();
+  }, []);
+
+  const SlideItem = ({item}) => {
+    // console.log(widthBanner);
+    // const widthBanner = 1;
+    return (
+      <View style={styles.bannerItem(widthBanner)}>
+        <Image
+          source={{
+            uri: item.gambar,
+            cache: 'only-if-cached',
+          }}
+          // source={images.logo_fs}
+          resizeMode="stretch"
+          style={styles.img}
+        />
+      </View>
+    );
+  };
+
   return (
     <View style={styles.banner}>
       <FlatList
@@ -44,19 +59,19 @@ const Banners = () => {
 const styles = StyleSheet.create({
   img: {
     // backgroundColor: COLORS.black,
+    // width: SIZES.width * 0.9,
     height: SIZES.height * 0.2,
   },
   banner: {
     marginVertical: 10,
   },
-  bannerItem: {
+  bannerItem: widthBanner => ({
     backgroundColor: COLORS.white,
     marginEnd: 10,
     flex: 1,
-    alignItems: 'baseline',
-
+    width: SIZES.width * widthBanner,
     height: SIZES.height * 0.2,
-  },
+  }),
 });
 
 export default Banners;
